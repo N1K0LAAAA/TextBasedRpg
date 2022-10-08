7a9f63d54a76a1b1d20026b2f3ac106af055f1b6
N1K0LAAAA <114262292+N1K0LAAAA@users.noreply.github.com>
2022-10-08 21:30:10 +0200

    Display Menu / Shop / Arena

--- RPG/Program.cs
+++ RPG/Program.cs
@@ -2,10 +2,56 @@
 { 
     public static void Main(string[] args)
     {
-        Mob sheep = new Mob("Sheep", 4, 1, 0);
-        Console.WriteLine(sheep);
-        Console.ReadLine();
+        displayMenu();
+       
+       
+       Console.ReadLine();
+
+    }
+    static void displayMenu()
+    {
+        Console.Clear();
+        Console.WriteLine("1) Arena");
+        Console.WriteLine("2) Shop");
+
+           switch(Console.ReadKey().Key)
+            {
+
+                case ConsoleKey.D1:
+                  displayArena();
+                    break;
+
+                case ConsoleKey.D2:
+                    displayShop();
+                break;
+           }
+    }
+    static void displayArena()
+    {
+        Console.Clear();
+        Console.WriteLine("Press Escape to go back to the main Menu");
+        Console.WriteLine("1");
+
+        switch (Console.ReadKey().Key)
+        {
+            case ConsoleKey.Escape:
+                displayMenu();
+                break;
+        }
+    }
+    static void displayShop()
+    {
         
+        Console.Clear();
+        Console.WriteLine("Press Escape to go back to the main Menu");
+        Console.WriteLine("Shop Items");
+
+        switch (Console.ReadKey().Key)
+        {
+            case ConsoleKey.Escape:
+                displayMenu();
+                break;
+        }
 
     }
 }
