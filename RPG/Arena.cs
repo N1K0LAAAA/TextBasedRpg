60f4916ae474fa5ffb51f191bb074829836cdcc7
N1K0LAAAA <114262292+N1K0LAAAA@users.noreply.github.com>
2022-10-10 18:54:13 +0200

    Arena / Shop Klasse mit constructor

---
+++ RPG/Arena.cs
@@ -0,0 +1,11 @@
+public class Arena
+{
+    int arena1bis10;
+    string arenaName;
+
+    public Arena(int arenaLevel, string name)
+    {
+        arena1bis10 = arenaLevel;
+        arenaName = name;
+    }
+}
