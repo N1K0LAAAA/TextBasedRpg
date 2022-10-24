001888b0ac518cee2e2e43951b1b2566170cb11d
Maxzimmerman <115488694+Maxzimmerman@users.noreply.github.com>
2022-10-24 12:05:19 +0200

    I created 10 Arena objects

--- RPG/Program.cs
+++ RPG/Program.cs
@@ -11,5 +11,16 @@ public class Program
         viewController.addView("Shop", new Shop());
 
         viewController.display("Menu");
+
+        Arena arena1 = new Arena(1, "Village");
+        Arena arena2 = new Arena(2, "Desert");
+        Arena arena3 = new Arena(3, "Mountain");
+        Arena arena4 = new Arena(4, "Field");
+        Arena arena5 = new Arena(5, "Forest");
+        Arena arena6 = new Arena(6, "City");
+        Arena arena7 = new Arena(7, "Ocean");
+        Arena arena8 = new Arena(8, "Goblin cave");
+        Arena arena9 = new Arena(9, "Zeppelin");
+        Arena arena10 = new Arena(10, "Volcano");
     }
 }
